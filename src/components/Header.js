c6d4135ad574b664f25752f2eaa3829ce165381c
Ignacio Pujols <49131161+ignemdev@users.noreply.github.com>
2022-04-03 18:54:06 -0400

    cambios en el header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,10 @@ const Header = () => {
         <Card variant="primary" sx={{ ...spacing, ...border }}>
             <CardContent sx={{ padding: 0 }}>
                 <Typography variant="h4" component="div" sx={{ marginBottom: 3, width: '100%', fontWeight: 'bold', textTransform: 'uppercase' }} >
-                    ¿Qué tan saludable eres?
+                    ¿Qué tan saludable te sientes?
                 </Typography>
                 <Typography variant="body2">
-                    Health Test With DBL
+                    ¡Descubre que tan saludable es tu estilo de vida! Simplemente responde las siguientes preguntas con honestidad. Siguenos en nuestras redes sociales para que conozcas más sobre nuestra campaña Better for you. ¡No olvides compartirnos tus resultados @dblmarket!
                 </Typography>
             </CardContent>
         </Card>
